d92f7474522658e43888034ba325a467f136e43f
Ardcord <93101315+Ardcord@users.noreply.github.com>
2022-03-15 10:41:15 +0100

    Update get_next_line.h

--- GetNextLine/get_next_line.h
+++ GetNextLine/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: tvanbael <tvanbael@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 13:44:56 by tvanbael          #+#    #+#             */
-/*   Updated: 2022/03/10 11:45:05 by tvanbael         ###   ########.fr       */
+/*   Updated: 2022/03/10 18:48:10 by tvanbael         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,17 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+typedef struct s_index
+{
+	int		i;
+	int		j;
+	char	*save;
+}			t_index;
+
+char	*get_next_line(int fd);
 int		ft_strchr(char *str, char c);
 int		ft_strlen(char *str);
-int		ft_strlenreste(char *str);
+int		ft_strreste(char *str);
 char	*ft_save_in_line(char *line, char *tmp);
 
 #endif
